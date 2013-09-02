2ba0200d8e712cd444c3fa9edb4474e4466de6fe
Lloyd Konneker <bootch@nc.rr.com>
2013-09-02 15:34:21 -0400

    Initial commit

---
+++ generateMSI/generateMSI.py
@@ -0,0 +1,114 @@
+'''
+Generate .msi from template via WiX tools.
+
+A scripting of a process.
+Where the process is 'generate an installer for Windows',
+Which is a subprocess of 'publish', a subprocess of 'develop software'.
+
+The scripting accomplishes:
+- edit the WiX source (so you don't have to understand WiX XML.)
+- automates calling of WiX tools
+- FUTURE parameterize with the version, etc. for redo or repeat
+It is a layer above the WiX layer.
+
+Capability: VERY LIMITED.
+This creates a very simple MSI, for a very simple case.
+In other words, it is hardcoded and inflexible except for a few parameters (see below)
+The same name is used ubuiquitously for the product, company, websites, certain directories, etc.
+It also installs a fixed set of components (see below.)
+In other words: specific to a company with a single product, where the product has its own mimetype.
+
+Requires of the machine it runs on:
+- Windows OS
+- WiX toolset installed
+- Python installed
+
+Preconditions: you have, in some directory (? same as where the script resides ?)
+- created a single binary for your app (say using PyInstaller)
+- created a single app icon (.ico) for your app and its mimetype
+- edit the template map so that it describes your app
+
+Postcondition:
+- a .msi file is created (or overwritten), named for the version
+
+To use: 
+- edit the template map (first time: all variables, subsequent times, just the version number?)
+- copy any update app.exe to the directory
+- python generateMSI.py
+- copy the generated .msi file to your distribution chain
+
+Variables:
+- appName used to name the binary and the .ico and the install directory
+- companyName must be used to name the company and its website e.g. company.org
+
+Components installed:
+- a single executable
+- a single icon (for both the app and its mimetype)
+- a single shortcut in the start menu
+(Notably missing help files, copyright file.)
+
+FUTURE
+------
+You MUST leave the generated source file (.wsc ) to serve as 'database' for subsequent uses.
+
+User interface on the command line (query and response.)
+
+Every use can be:
+- initial run for this app (requires you to cut and paste a GUID, say from guid.com)
+- redo of previous run (when initial generated MSI was found flawed in testing.)
+- subsequent run for a another release of same app, generates another named .msi
+(e.g. increments the bug fix version number)
+
+'''
+from string import Template
+from subprocess import call
+
+import templateWiX
+
+
+def createFilename(templateString):
+  # fab filename from template string and template map
+  template = Template(templateString)
+  return template.substitute(templateWiX.TEMPLATE_MAP)
+
+
+def createWiXSourceFromTemplate():
+  template = Template(templateWiX.WIX_TEMPLATE)
+  return template.substitute(templateWiX.TEMPLATE_MAP)
+
+
+def generateMSIFromWIX(source):
+  
+  
+  outFilename = createFilename(templateWiX.OUT_FILENAME_TEMPLATE)
+  sourceFileName = createFilename(templateWiX.SOURCE_FILENAME_TEMPLATE)
+  intermediateFileName = createFilename(templateWiX.INTERMEDIATE_FILENAME_TEMPLATE)
+  
+  # Write source xml text to file
+  with open(sourceFileName, 'w') as f:
+    f.write(source)
+    
+  # invoke WiX tools in a chain through intermediate
+  try:
+    # WiX is two tools in sequence with an intermediate file
+    call(["candle", sourceFileName])
+    # ask light to rename its output file from the default
+    call(["light", "-out " + outFilename + " " + intermediateFileName])
+  except OSError:
+    print "Is WiX toolset installed and in PATH?"
+    raise
+  print "Generated in current directory:", outFilename
+
+
+def main():
+  
+  # FUTURE UI: dialog to figure out what kind of run, then futz with guid and version in template map
+  
+  WiXSource = createWiXSourceFromTemplate()
+  print WiXSource
+  generateMSIFromWIX(WiXSource)
+  
+  
+  
+if __name__ == '__main__':
+    main()
---
+++ generateMSI/templateWiX.py
@@ -0,0 +1,85 @@
+'''
+template for a WiX input file
+
+Note that $$ is string.Template's escape mechanism: yields a single $ (for use in WiX variables.)
+'''
+
+# Can't user 0.0.1-beta for version???
+
+TEMPLATE_MAP = { \
+'appName'  : 'pensool',
+'companyName' : 'Pensool.org',
+'guid' : '12345678-1234-1234-1234',
+'version' : '0.0.1',
+'mimetypeExtension' : 'pnl'
+ }
+
+SOURCE_FILENAME_TEMPLATE = r'''${appName}.msc'''
+INTERMEDIATE_FILENAME_TEMPLATE = r'''${appName}.mso'''
+OUT_FILENAME_TEMPLATE = r'''${appName}_$version.msi'''
+
+
+
+WIX_TEMPLATE = r''' <?xml version="1.0"?>
+<?define ProductVersion = "$version"?>
+<?define ProductUpgradeCode = "$guid-111111111111"?>
+<Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">
+   <Product Id="*" UpgradeCode="$$(var.ProductUpgradeCode)" 
+            Name="appName" Version="$$(var.ProductVersion)" Manufacturer="$companyName" Language="1033">
+      <Package InstallerVersion="200" Compressed="yes" Comments="Windows Installer Package"/>
+      <Media Id="1" Cabinet="product.cab" EmbedCab="yes"/>
+      <Icon Id="ProductIcon" SourceFile="$appName.ico"/>
+      <Property Id="ARPPRODUCTICON" Value="ProductIcon"/>
+      <Property Id="ARPHELPLINK" Value="http://www.$companyName/help"/>
+      <Property Id="ARPURLINFOABOUT" Value="http://www.$companyName/about"/>
+      <Property Id="ARPNOREPAIR" Value="1"/>
+      <Property Id="ARPNOMODIFY" Value="1"/>
+      <Upgrade Id="$$(var.ProductUpgradeCode)">
+         <UpgradeVersion Minimum="$$(var.ProductVersion)" OnlyDetect="yes" Property="NEWERVERSIONDETECTED"/>
+         <UpgradeVersion Minimum="0.0.0" Maximum="$$(var.ProductVersion)" IncludeMinimum="yes" IncludeMaximum="no" 
+                         Property="OLDERVERSIONBEINGUPGRADED"/>    
+      </Upgrade>
+      <Condition Message="A newer version of this software is already installed.">NOT NEWERVERSIONDETECTED</Condition>
+ 
+      <!-- components copied/created on install machine -->
+      <Directory Id="TARGETDIR" Name="SourceDir">
+         <Directory Id="ProgramFilesFolder">
+            <Directory Id="INSTALLDIR" Name="$appName">
+               <Component Id="ApplicationFiles" Guid="$guid-222222222222">
+                  <File Id="ApplicationFile1" Source="$appName.exe"/>
+               </Component>
+            </Directory>
+         </Directory>
+ 
+         <Directory Id="ProgramMenuFolder">
+            <Directory Id="ProgramMenuSubfolder" Name="$appName">
+               <Component Id="ApplicationShortcuts" Guid="$guid-333333333333">
+                  <Shortcut Id="ApplicationShortcut1" Name="$appName" Description="$appName" 
+                            Target="[INSTALLDIR]$appName.exe" WorkingDirectory="INSTALLDIR"/>
+                  <RegistryValue Root="HKCU" Key="Software\$companyName\$appName" 
+                            Name="installed" Type="integer" Value="1" KeyPath="yes"/>
+                  <RemoveFolder Id="ProgramMenuSubfolder" On="uninstall"/>
+               </Component>
+            </Directory>
+         </Directory>
+      </Directory>
+      
+      <!-- register mimetype.  Probably not safe from conflict with existing mimetypes. -->
+      <ProgId Id="$appName.$mimetypeExtension" Description="$appName file type">
+         <Extension Id="$mimetypeExtension" ContentType="application/$mimetypeExtension">
+            <Verb Id="open" Command="open" TargetFile="$appName.exe" Argument='"%1"'/>
+         </Extension>
+      </ProgId>
+ 
+      <InstallExecuteSequence>
+         <RemoveExistingProducts After="InstallValidate"/>
+      </InstallExecuteSequence>
+ 
+      <Feature Id="DefaultFeature" Level="1">
+         <ComponentRef Id="ApplicationFiles"/>
+         <ComponentRef Id="ApplicationShortcuts"/>     
+      </Feature>
+   </Product>
+</Wix>
+
+'''
